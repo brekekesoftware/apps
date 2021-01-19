11edee55f7d7e3a33d6511172b4d81085863000a
Nam Nguyen <nam@namnm.com>
2021-01-20 02:39:46 +0700

    Handle both .phone and .phonedev

--- src/components/PageDev.js
+++ src/components/PageDev.js
@@ -4,8 +4,20 @@ import LastModified from '../shared/LastModified';
 
 const PageDev = () => (
   <React.Fragment>
-    <h1>Brekeke Phone dev</h1>
-    <h3>New UI:</h3>
+    <h3>Brekeke Phone Dev</h3>
+    <a href="itms-services://?action=download-manifest&url=https://apps.brekeke.com/0/brekeke_phonedev.plist">
+      <span>
+        Install on iOS <LastModified url="/0/brekeke_phonedev.ipa" />
+      </span>
+    </a>
+    <br />
+    <a href="/0/brekeke_phonedev.apk">
+      <span>
+        Download for Android <LastModified url="/0/brekeke_phonedev.apk" />
+      </span>
+    </a>
+    <br />
+    <h3>Brekeke Phone</h3>
     <a href="itms-services://?action=download-manifest&url=https://apps.brekeke.com/0/brekeke_phone.plist">
       <span>
         Install on iOS <LastModified url="/0/brekeke_phone.ipa" />
@@ -18,21 +30,9 @@ const PageDev = () => (
       </span>
     </a>
     <br />
-    <a href="/0/brekeke_phone_web.zip">
+    <a href="/0/brekeke_phonedev_web.zip">
       <span>
-        Web zip <LastModified url="/0/brekeke_phone_web.zip" />
-      </span>
-    </a>
-    <h3>Old UI v1:</h3>
-    <a href="itms-services://?action=download-manifest&url=https://apps.brekeke.com/0/brekeke_phone_v1.plist">
-      <span>
-        Install on iOS <LastModified url="/0/brekeke_phone_v1.ipa" />
-      </span>
-    </a>
-    <br />
-    <a href="/0/brekeke_phone_v1.apk">
-      <span>
-        Download for Android <LastModified url="/0/brekeke_phone_v1.apk" />
+        Web zip <LastModified url="/0/brekeke_phone.zip" />
       </span>
     </a>
   </React.Fragment>
